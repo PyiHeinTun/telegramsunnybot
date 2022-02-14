200c218772a63cb8e87d1266be2a70d1545259b9
PyiHeinTun <pyiheintun1998@gmail.com>
2022-02-14 16:33:04 +0630

    UPTOBOTH

--- bot.py
+++ bot.py
@@ -54,7 +54,8 @@ async def _main(_, message):
             [
                 [
                  InlineKeyboardButton("Upload to Server 1", callback_data="uptostreamtape"),
-                 InlineKeyboardButton("Upload to Server 2", callback_data="uptofembed")
+                 InlineKeyboardButton("Upload to Server 2", callback_data="uptofembed"),
+                 InlineKeyboardButton("Upload to Both", callback_data="uptoboth")
                 ]
             ]
         ),
@@ -113,15 +114,10 @@ async def button(bot, data: CallbackQuery):
                 parsed_html = BeautifulSoup(html, features="html.parser")
                 id = parsed_html.body.find('textarea', attrs={'name':'fn'}).text
                 await data.message.reply_to_message.reply_text(
-                   f"**File Name:** `{filename}`\n\n**Server 1 Video Link  :**   `{id}`",
+                   f"**File Name:** `{filename}`\n\n**Download Link:** `{id}`",
                    parse_mode="Markdown",
                    disable_web_page_preview=True,
                    )
-               # forwarded_msg = await data.message.reply_to_message.forward(Config.LOG_CHANNEL)
-               # await bot.send_message(chat_id=Config.LOG_CHANNEL,
-               #                     text=f"#STREAMTAPE_UPLOAD:\n\n[{data.from_user.first_name}](tg://user?id={data.from_user.id}) Uploaded to Streamsb !!\n\n**URL:** {download_link}",
-               #                     reply_to_message_id=forwarded_msg.message_id, parse_mode="Markdown",
-               #                       disable_web_page_preview=True)
     elif "uptofembed" in cb_data:
         downloadit = data.message.reply_to_message
         a = await data.message.edit("Downloading to my Server ...", parse_mode="Markdown",
@@ -180,11 +176,96 @@ async def button(bot, data: CallbackQuery):
                     await b.delete(True)
                     await c.delete(True)
                     await data.message.reply_to_message.reply_text(
-                    f"**File Name:**  `{filename}` \n\n**Server 2 Video Link  :** ` {rawJson['data']}`",
+                    f"**File Name:**  `{filename}` \n\n**Video ID:** `{rawJson['data']}`",
                     parse_mode="Markdown",
                     disable_web_page_preview=True,
                     )
                     break
+                    
+    elif "uptoboth" in cb_data:
+        downloadit = data.message.reply_to_message
+        a = await data.message.edit("Downloading to my Server ...", parse_mode="Markdown",
+                                    disable_web_page_preview=True)
+        dl_loc = Config.DOWNLOAD_DIR + "/" + str(data.from_user.id) + "/"
+        if not os.path.isdir(dl_loc):
+            os.makedirs(dl_loc)
+        c_time = time.time()
+        the_media = await bot.download_media(
+            message=downloadit,
+            file_name=dl_loc,
+            progress=progress_for_pyrogram,
+            progress_args=(
+                "Downloading Video ...",
+                a,
+                c_time
+            )
+        )
+        b = await data.message.edit("Uploading to my Server ...", parse_mode="Markdown",
+                                    disable_web_page_preview=True)
+    
+        async with aiohttp.ClientSession() as session:
+            #UPLOAD TO STREAMSB
+            Main_API = "https://api.streamsb.com/api/upload/server?key={key}"
+            hit_api = await session.get(Main_API.format(key = Config.STREAMSB_API))
+            json_data = await hit_api.json()
+            temp_api = json_data["result"]
+            files = {'file': open(the_media, 'rb'),'api_key': Config.STREAMSB_API}
+            response = await session.post(temp_api, data=files)
+            filename = the_media.split("/")[-1].replace("_", " ")
+            
+            if not int(response.status) == 200:
+                await data.message.reply_to_message.reply_text(
+                    "Something Went Wrong!\n\n**Error:** Server Didn't Accept My Request!", parse_mode="Markdown",
+                    disable_web_page_preview=True)
+                return
+                
+            #UPLOAD TO FEMBED
+            uploadData = {'client_id':'383227', 'client_secret': Config.FEMBED_API}
+            uploadUrl = await session.post('https://www.fembed.com/api/upload',data=uploadData)
+            uploadUrlExtract = await uploadUrl.json()
+            temp_url = uploadUrlExtract['data']['url']
+            temp_token = uploadUrlExtract['data']['token']
+            
+            filename = the_media.split("/")[-1].replace("_", " ")
+            creation_url = temp_url
+            metadata = {
+                "token": str(temp_token).encode(),
+                "name": str(filename).encode()
+            }
+            # Upload a file to a tus server.
+            with open(the_media, "rb") as f:
+                location = await aiotus.upload(creation_url, f, metadata)
+            c = await data.message.edit("Encoding Video Please Wait.....", parse_mode="Markdown",
+                                    disable_web_page_preview=True)
+            while True:
+                id_temp_url = 'https://www.fembed.com/api/fingerprint'
+                file_fingerprint = str(location).split('upload/')[1]
+                files = {'client_id': '383227','client_secret': Config.FEMBED_API , 'file_fingerprint': file_fingerprint}
+                fembedresponse = await session.post(id_temp_url, data=files)
+                print(file_fingerprint)
+                rawJson = await fembedresponse.json()
+                print(rawJson['data'])
+                if(rawJson['data'] == "Fingerprint not found"):
+                    time.sleep(60)
+                else:
+                    try:
+                        os.remove(the_media)
+                    except:
+                        pass
+                    await a.delete(True)
+                    await b.delete(True)
+                    html = str(await response.text())
+                    parsed_html = BeautifulSoup(html, features="html.parser")
+                    id = parsed_html.body.find('textarea', attrs={'name':'fn'}).text
+                    await a.delete(True)
+                    await b.delete(True)
+                    await c.delete(True)
+                    await data.message.reply_to_message.reply_text(
+                    f"**File Name:**  `{filename}` \n\n**Server 1:** `{id}` \n\n**Server 2** `{rawJson['data']}`",
+                    parse_mode="Markdown",
+                    disable_web_page_preview=True,
+                    )
+                    break 
 
     elif "showcreds" in cb_data:
         if int(data.from_user.id) == Config.BOT_OWNER:
