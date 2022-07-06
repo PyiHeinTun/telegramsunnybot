526d49f9d6b67c3a9eeab04321f9a0d25c684270
PyiHeinTun <pyiheintun1998@gmail.com>
2022-07-06 09:10:45 +0630

    fe

--- configs.py
+++ configs.py
@@ -3,15 +3,15 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
+	API_ID = os.environ.get("API_ID")
 	API_ID = os.environ.get("API_ID")
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	STREAMSB_API = os.environ.get("STREAMSB_API")
 	FEMBED_API = os.environ.get("FEMBED_API")
 	SESSION_NAME = os.environ.get("SESSION_NAME", "CloudManagerBot")
-	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	BOT_OWNER = os.environ.get("BOT_OWNER")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
 	DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
 	HELP_TEXT = """
 Send me any Media & Choose Upload Server,
